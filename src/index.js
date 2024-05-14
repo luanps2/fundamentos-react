19c64936f662ee7a3675ab63dbdf649194761a2b
luanps2 <luan-esposito@hotmail.com>
2024-05-14 19:43:48 -0300

    Primeiro projeto react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'; //dom
+import React from 'react'; //react
+import './index.css'; //estilo
+import Primeiro from './components/basicos/Primeiro.js'
+
+
+ReactDOM.render( 
+    <div>
+       <Primeiro></Primeiro>
+
+    </div>, 
+    document.getElementById('root')
+)
