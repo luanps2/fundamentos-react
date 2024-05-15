3f20cc65f1a92ddfd9f0553b314ea0fd4fc0ac23
luanps2 <luan-esposito@hotmail.com>
2024-05-14 21:39:15 -0300

    componentes e condicional

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,21 @@
+import React from 'react' // depreciado, não é necessário
+export default function ComParametro(props){
+    // const sub = props.subtitulo
+    console.log(props)
+    const status = props.nota>=7?'Aprovado' : 'Reprovado';
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+
+            <p>
+                <strong> {props.aluno } </strong> 
+                 tem nota  
+                <strong> {props.nota} </strong>
+                e está
+                <strong> { status } </strong>
+            </p>
+            
+            
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import ReactDOM from 'react-dom'; //dom
 import React from 'react'; //react
 import './index.css'; //estilo
-import Primeiro from './components/basicos/Primeiro.js'
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
-ReactDOM.render( 
-    <div>
-       <Primeiro></Primeiro>
+ReactDOM.render(
+    <div id="app">
+        <Primeiro></Primeiro>
+        <ComParametro 
+        titulo="Situação do Aluno"
+        aluno="Pedro Silva " nota={9.3}/>
+        <ComParametro 
+        titulo="Situação do Aluno"
+        aluno="Maria Simone" 
+        nota={5}/>
 
-    </div>, 
+     
+
+    </div>,
     document.getElementById('root')
 )
