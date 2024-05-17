26162b8443ea5620a2fa5bdd2fd6abad0f8dc32d
Luan Costa PC <luan-esposito@hotmail.com>
2024-05-17 01:17:24 -0300

    Cards responsivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-
-import Card from './components/layout/Card'
+import './App.css'
+import Card from "./components/layout/Card";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
@@ -10,26 +10,26 @@ export default () => (
   <div id="app">
     <h1>Fundamentos React </h1>
 
-    <Card titulo="Desafio Aleatório">
-      <Aleatorio min={0} max={60} />
-    </Card>
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={0} max={60} />
+      </Card>
 
-    <Card titulo="Fragmento">
-      <Fragmento />
-    </Card>
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
 
-    <Card titulo="Com Parâmetro">
-      <ComParametro titulo="Situação do Aluno" aluno="Pedro Silva " nota={8.5} />
-    </Card>
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Pedro Silva "
+          nota={8.5}
+        />
+      </Card>
 
-    <Card titulo="Primeiro Componente">
-
-      <Primeiro></Primeiro>
-    </Card>
-
-
-
-
-
+      <Card titulo="#01 - Primeiro Componente">
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
   </div>
 );
